0289cb1a5544e7c875198bfa155cfa51c9c8fbfe
kresimir.spes <kresimir.spes@ea0b51eb-d998-e50a-db7b-7bccbe951d2a>
2010-04-21 10:34:53 +0000

    * License information

--- trunk/src/DummySound.cpp
+++ trunk/src/DummySound.cpp
@@ -1,3 +1,12 @@
+/************************************************************************************\
+This source file is part of the KS(X) audio library                                  *
+For latest info, see http://libatres.sourceforge.net/                                *
+**************************************************************************************
+Copyright (c) 2010 Kresimir Spes (kreso@cateia.com)                                  *
+*                                                                                    *
+* This program is free software; you can redistribute it and/or modify it under      *
+* the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php   *
+\************************************************************************************/
 #include "DummySound.h"
 #include "SoundManager.h"
 
