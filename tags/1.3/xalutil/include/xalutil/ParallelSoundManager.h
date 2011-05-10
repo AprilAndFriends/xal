082f397df1fe2e731900d487cdc11542faf2d5a4
boris.blizzard <boris.blizzard@ea0b51eb-d998-e50a-db7b-7bccbe951d2a>
2011-05-10 08:29:40 +0000

    - v1.3 - tag of stable version before XAL 2.0

---
+++ tags/1.3/xalutil/include/xalutil/ParallelSoundManager.h
@@ -0,0 +1,46 @@
+/************************************************************************************\
+This source file is part of the KS(X) audio library                                  *
+For latest info, see http://code.google.com/p/libxal/                                *
+**************************************************************************************
+Copyright (c) 2010 Kresimir Spes, Boris Mikic, Ivan Vucica                           *
+*                                                                                    *
+* This program is free software; you can redistribute it and/or modify it under      *
+* the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php   *
+\************************************************************************************/
+#ifndef XAL_PARALLELSOUNDMANAGER_H
+#define XAL_PARALLELSOUNDMANAGER_H
+
+#include <hltypes/harray.h>
+#include <hltypes/hstring.h>
+
+#include "xalUtilExport.h"
+
+namespace xal
+{
+	class xalUtilExport ParallelSoundManager
+	{
+	public:
+		ParallelSoundManager(float fadeTime = 1.0f);
+		~ParallelSoundManager();
+		
+		float getFadeTime() { return this->fadeTime; }
+		void setFadeTime(float value) { this->fadeTime = value; }
+		
+		void addSound(chstr name);
+		void updateList();
+		void updateList(harray<hstr> names);
+		void playAll();
+		void pauseAll();
+		void stopAll();
+		void clear();
+		
+	protected:
+		float fadeTime;
+		harray<hstr> sounds;
+		harray<hstr> buildList;
+	
+	};
+	
+}
+
+#endif
