453d5d9a00e6d0465eb9878117f739928ebaa789
Boris Mikic <boris.blizzard@gmail.com>
2011-08-18 16:13:37 +0000

    - added empty DirectSound_MIDI_Source

---
+++ audiosystems/DirectSound/DirectSound_MIDI_Source.cpp
@@ -0,0 +1,23 @@
+/// @file
+/// @author  Boris Mikic
+/// @version 2.0
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+
+#if HAVE_DIRECTSOUND && HAVE_MIDI
+#include <windows.h>
+
+#include <hltypes/hfile.h>
+#include <hltypes/hstring.h>
+
+#include "AudioManager.h"
+#include "DirectSound_MIDI_Source.h"
+#include "xal.h"
+
+namespace xal
+{
+}
+#endif
