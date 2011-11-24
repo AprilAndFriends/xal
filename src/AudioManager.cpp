f41d7153e84e5a24fbec2d633e29f3a1cdc90885
Boris Mikic <boris.blizzard@gmail.com>
2011-11-24 08:25:13 +0000

    - added NULLing of xal::mgr while it does not exist

--- src/AudioManager.cpp
+++ src/AudioManager.cpp
@@ -43,7 +43,7 @@
 
 namespace xal
 {
-	AudioManager* mgr;
+	AudioManager* mgr = NULL;
 
 	AudioManager::AudioManager(chstr systemName, unsigned long backendId, bool threaded, float updateTime, chstr deviceName) :
 		enabled(false), paused(false), gain(1.0f), thread(NULL)
