5a3125733ef22fc4330ca6f945cce883285caccd
Boris Mikic <boris.blizzard@gmail.com>
2011-11-25 13:11:50 +0000

    - added MultiPlaylist

---
+++ xalutil/src/MultiPlaylist.cpp
@@ -0,0 +1,119 @@
+/// @file
+/// @author  Boris Mikic
+/// @version 2.0
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+
+#include <hltypes/exception.h>
+#include <hltypes/harray.h>
+
+#include "MultiPlaylist.h"
+#include "Playlist.h"
+
+namespace xal
+{
+	MultiPlaylist::MultiPlaylist() : enabled(true)
+	{
+	}
+	
+	MultiPlaylist::~MultiPlaylist()
+	{
+		this->clear();
+	}
+
+	bool MultiPlaylist::isPlaying()
+	{
+		foreach (Playlist*, it, this->playlists)
+		{
+			if ((*it)->isPlaying())
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	bool MultiPlaylist::isPaused()
+	{
+		foreach (Playlist*, it, this->playlists)
+		{
+			if (!(*it)->isPaused())
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	void MultiPlaylist::registerPlaylist(Playlist* playlist)
+	{
+		if (this->playlists.contains(playlist))
+		{
+			throw hl_exception("Error! Playlist was already registered!");
+		}
+		this->playlists += playlist;
+	}
+
+	void MultiPlaylist::unregisterPlaylist(Playlist* playlist)
+	{
+		if (!this->playlists.contains(playlist))
+		{
+			throw hl_exception("Error! Playlist has not been registered!");
+		}
+		this->playlists -= playlist;
+	}
+		
+	void MultiPlaylist::clear()
+	{
+		this->stop();
+		foreach (Playlist*, it, this->playlists)
+		{
+			delete (*it);
+		}
+		this->playlists.clear();
+	}
+	
+	void MultiPlaylist::play(float fadeTime)
+	{
+		foreach (Playlist*, it, this->playlists)
+		{
+			(*it)->play(fadeTime);
+		}
+	}
+	
+	void MultiPlaylist::stop(float fadeTime)
+	{
+		foreach (Playlist*, it, this->playlists)
+		{
+			(*it)->stop(fadeTime);
+		}
+	}
+	
+	void MultiPlaylist::pause(float fadeTime)
+	{
+		foreach (Playlist*, it, this->playlists)
+		{
+			(*it)->pause(fadeTime);
+		}
+	}
+
+	void MultiPlaylist::shuffle()
+	{
+		foreach (Playlist*, it, this->playlists)
+		{
+			(*it)->shuffle();
+		}
+	}
+
+	void MultiPlaylist::reset()
+	{
+		foreach (Playlist*, it, this->playlists)
+		{
+			(*it)->reset();
+		}
+	}
+	
+}
