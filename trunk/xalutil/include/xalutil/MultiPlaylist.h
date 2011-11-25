6a16ddfd26785371e45a117c5054993556d99e6b
boris.blizzard <boris.blizzard@ea0b51eb-d998-e50a-db7b-7bccbe951d2a>
2011-11-25 13:11:50 +0000

    - added MultiPlaylist

---
+++ trunk/xalutil/include/xalutil/MultiPlaylist.h
@@ -0,0 +1,56 @@
+/// @file
+/// @author  Boris Mikic
+/// @version 2.0
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+/// 
+/// @section DESCRIPTION
+/// 
+/// Represents a multi playlist.
+
+#ifndef XAL_MULTI_PLAYLIST_H
+#define XAL_MULTI_PLAYLIST_H
+
+#include <hltypes/harray.h>
+
+#include "xalUtilExport.h"
+
+namespace xal
+{
+	class Playlist;
+
+	class xalUtilExport MultiPlaylist
+	{
+	public:
+		MultiPlaylist();
+		~MultiPlaylist();
+		
+		bool isEnabled() { return this->enabled; }
+		void setEnabled(bool value) { this->enabled = value; }
+		harray<Playlist*> getPlaylists() { return this->playlists; }
+		bool isPlaying();
+		bool isPaused();
+
+		void registerPlaylist(Playlist* playlist);
+		void unregisterPlaylist(Playlist* playlist);
+		
+		void clear();
+		
+		void play(float fadeTime = 0.0f);
+		void stop(float fadeTime = 0.0f);
+		void pause(float fadeTime = 0.0f);
+		void shuffle();
+		void reset();
+		
+	protected:
+		bool enabled;
+		harray<Playlist*> playlists;
+	
+	};
+	
+}
+
+#endif
