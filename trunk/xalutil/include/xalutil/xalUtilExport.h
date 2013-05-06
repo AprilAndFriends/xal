7d7838813b15fdd2d33f75e48e9e708706d77ca2
boris.blizzard <boris.blizzard@ea0b51eb-d998-e50a-db7b-7bccbe951d2a>
2013-05-06 13:08:57 +0000

    - removed Android Audio system (use OpenAL instead) - updated VS 2012 projects - added static build configurations

--- trunk/xalutil/include/xalutil/xalUtilExport.h
+++ trunk/xalutil/include/xalutil/xalUtilExport.h
@@ -18,7 +18,7 @@
 	/// @brief Macro for DLL exports/imports.
 	/// @def xalUtilFnExport
 	/// @brief Macro for function DLL exports/imports.
-	#ifdef _STATICLIB
+	#ifdef _LIB
 		#define xalUtilExport
 		#define xalUtilFnExport
 	#else
