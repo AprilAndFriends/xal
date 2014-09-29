e59e933404bff94d55c673fe7ba64b74db6a514d
Boris Mikic <boris.blizzard@gmail.com>
2014-09-29 18:08:24 +0000

    - Android build fixes

--- src/BufferAsync.cpp
+++ src/BufferAsync.cpp
@@ -6,6 +6,10 @@
 /// This program is free software; you can redistribute it and/or modify it under
 /// the terms of the BSD license: http://opensource.org/licenses/BSD-3-Clause
 
+#ifdef _ANDROID
+#include <unistd.h>
+#endif
+
 #include <hltypes/harray.h>
 #include <hltypes/hmap.h>
 #include <hltypes/hmutex.h>
