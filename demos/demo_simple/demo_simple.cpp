1befe54df42b89b03411bea7d0ea5a5c868a7bed
Boris Mikic <boris.blizzard@gmail.com>
2016-12-28 12:27:06 +0100

    - demos update

--- demos/demo_simple/demo_simple.cpp
+++ demos/demo_simple/demo_simple.cpp
@@ -19,6 +19,9 @@
 #endif
 
 #include <stdio.h>
+#if defined(_WIN32) && !defined(_WINRT)
+#include <windows.h>
+#endif
 
 #include <hltypes/hlog.h>
 #include <hltypes/hstring.h>
@@ -57,20 +60,20 @@ int main(Platform::Array<Platform::String^>^ args)
 	hwnd = GetConsoleWindow();
 #endif
 	// initialize XAL with platform default audio system, threaded update 100 times per second (every 0.01 seconds)
-	xal::init(xal::AS_DEFAULT, hwnd, __XAL_THREADED, 0.01f);
+	xal::init(xal::AudioSystemType::Default, hwnd, __XAL_THREADED, 0.01f);
 
 	// create a category for fully loaded and decoded sounds
-	xal::mgr->createCategory(CATEGORY_NORMAL, xal::ON_DEMAND, xal::DISK);
+	xal::manager->createCategory(CATEGORY_NORMAL, xal::BufferMode::OnDemand, xal::SourceMode::Disk);
 	// create a category for streamed sounds
-	xal::mgr->createCategory(CATEGORY_STREAMED, xal::STREAMED, xal::DISK);
+	xal::manager->createCategory(CATEGORY_STREAMED, xal::BufferMode::Streamed, xal::SourceMode::Disk);
 	// create sound using a prefix (forces usage of the ogg files)
-	xal::mgr->createSound(RESOURCE_PATH SOUND_NAME_NORMAL ".ogg", CATEGORY_NORMAL);
+	xal::manager->createSound(RESOURCE_PATH SOUND_NAME_NORMAL ".ogg", CATEGORY_NORMAL);
 	// create streamed sound using no prefix (forces usage of the ogg files)
-	xal::mgr->createSound(RESOURCE_PATH "streamable/" SOUND_NAME_STREAMED ".ogg", CATEGORY_STREAMED, PREFIX);
+	xal::manager->createSound(RESOURCE_PATH "streamable/" SOUND_NAME_STREAMED ".ogg", CATEGORY_STREAMED, PREFIX);
 	xal::Player* p = NULL;
 	
 	// create a sound player for manual control
-	p = xal::mgr->createPlayer(SOUND_NORMAL);
+	p = xal::manager->createPlayer(SOUND_NORMAL);
 	// play the sound
 	hlog::write("", "- starting " SOUND_NORMAL);
 	p->play();
@@ -79,14 +82,14 @@ int main(Platform::Array<Platform::String^>^ args)
 	{
 		hlog::writef("", "    - " SOUND_NORMAL " - samples: %d - time: %f", p->getSamplePosition(), p->getTimePosition());
 		hthread::sleep(100);
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
 	hlog::write("", "- finished " SOUND_NORMAL);
 	// destroy the player
-	xal::mgr->destroyPlayer(p);
+	xal::manager->destroyPlayer(p);
 
 	// create a new sound player
-	p = xal::mgr->createPlayer(SOUND_STREAMED);
+	p = xal::manager->createPlayer(SOUND_STREAMED);
 	// play the sound
 	hlog::write("", "- starting " SOUND_STREAMED);
 	p->play();
@@ -94,25 +97,25 @@ int main(Platform::Array<Platform::String^>^ args)
 	{
 		hlog::writef("", "    - " SOUND_STREAMED " - samples: %d - time: %f", p->getSamplePosition(), p->getTimePosition());
 		hthread::sleep(100);
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
 	hlog::write("", "- finished " SOUND_STREAMED);
 	// destroy the player
-	xal::mgr->destroyPlayer(p);
+	xal::manager->destroyPlayer(p);
 
 	// fire & forget, no control over the sound
-	xal::mgr->play(SOUND_NORMAL);
+	xal::manager->play(SOUND_NORMAL);
 	hlog::write("", "- starting " SOUND_NORMAL);
-	while (xal::mgr->isAnyPlaying(SOUND_NORMAL))
+	while (xal::manager->isAnyPlaying(SOUND_NORMAL))
 	{
 		hthread::sleep(100);
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
 	hlog::write("", "- finished " SOUND_NORMAL);
 
 	// destroying the sounds manually
-	xal::mgr->destroySound(xal::mgr->getSound(SOUND_NORMAL));
-	xal::mgr->destroySound(xal::mgr->getSound(SOUND_STREAMED));
+	xal::manager->destroySound(xal::manager->getSound(SOUND_NORMAL));
+	xal::manager->destroySound(xal::manager->getSound(SOUND_STREAMED));
 	// destroying XAL itself
 	xal::destroy();
 #ifndef _WINRT
--- demos/demo_test/demo_test.cpp
+++ demos/demo_test/demo_test.cpp
@@ -76,9 +76,9 @@
 #define MEMORY_MANGEMENT_ENABLED false
 #endif
 #ifdef _USE_RAM_SOURCES
-#define SOURCE_MODE xal::RAM
+#define SOURCE_MODE xal::SourceMode::Ram
 #else
-#define SOURCE_MODE xal::DISK
+#define SOURCE_MODE xal::SourceMode::Disk
 #endif
 
 #define OPENAL_MAX_SOURCES 16 // needed when using OpenAL
@@ -90,7 +90,7 @@ void _test_basic(xal::Player* player)
 	while (player->isPlaying())
 	{
 		hthread::sleep(100);
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
 }
 
@@ -101,66 +101,66 @@ void _test_sound(xal::Player* player)
 	for_iter (i, 0, 20)
 	{
 		hthread::sleep(100);
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 		player->pause();
 
 		hlog::writef("", "  - PAUSE: %02d - %05d %7.3f", i, player->getSamplePosition(), player->getTimePosition());
 
 		hthread::sleep(100);
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 		player->play();
 
 		hlog::writef("", "  - PLAY:  %02d - %05d %7.3f", i, player->getSamplePosition(), player->getTimePosition());
 	}
-	xal::mgr->update(1.0f);
+	xal::manager->update(1.0f);
 }
 
 void _test_multiplay(xal::Player* player)
 {
 	hlog::write("", "  - start test multiple play...");
-	xal::mgr->play(S_BARK);
+	xal::manager->play(S_BARK);
 	hthread::sleep(100);
-	xal::mgr->play(S_BARK);
-	while (xal::mgr->isAnyPlaying(S_BARK))
+	xal::manager->play(S_BARK);
+	while (xal::manager->isAnyPlaying(S_BARK))
 	{
 		hthread::sleep(100);
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
 	hthread::sleep(500);
 	hlog::write("", "starting stop test");
-	xal::mgr->play(S_WIND);
+	xal::manager->play(S_WIND);
 	hthread::sleep(200);
-	xal::mgr->play(S_WIND);
+	xal::manager->play(S_WIND);
 	int count = 0;
-	while (xal::mgr->isAnyPlaying(S_WIND))
+	while (xal::manager->isAnyPlaying(S_WIND))
 	{
 		hlog::writef("", "- wind stop iteration: %d", count);
 		for_iter (i, 0, 5)
 		{
 			hthread::sleep(100);
-			xal::mgr->update(0.1f);
+			xal::manager->update(0.1f);
 		}
-		xal::mgr->stop(S_WIND);
+		xal::manager->stop(S_WIND);
 		count++;
 	}
 	hthread::sleep(500);
 	hlog::write("", "starting stopFirst test");
-	xal::mgr->play(S_WIND);
+	xal::manager->play(S_WIND);
 	hthread::sleep(200);
-	xal::mgr->play(S_WIND);
+	xal::manager->play(S_WIND);
 	count = 0;
-	while (xal::mgr->isAnyPlaying(S_WIND))
+	while (xal::manager->isAnyPlaying(S_WIND))
 	{
 		hlog::writef("", "- wind stopFirst iteration: %d", count);
 		for_iter (i, 0, 5)
 		{
 			hthread::sleep(100);
-			xal::mgr->update(0.1f);
+			xal::manager->update(0.1f);
 		}
-		xal::mgr->stopFirst(S_WIND);
+		xal::manager->stopFirst(S_WIND);
 		count++;
 	}
-	xal::mgr->update(0.1f);
+	xal::manager->update(0.1f);
 }
 
 void _test_handle_stream(xal::Player* player)
@@ -168,13 +168,13 @@ void _test_handle_stream(xal::Player* player)
 	hlog::write("", "  - start test handle stream...");
 	player->play();
 	hthread::sleep(200);
-	xal::mgr->update(0.2f);
+	xal::manager->update(0.2f);
 	for_iter (i, 0, 5)
 	{
 		hlog::writef("", "  - play %d", i);
 		player->play();
 		hthread::sleep(1000);
-		xal::mgr->update(1.0f);
+		xal::manager->update(1.0f);
 		if (i == 1)
 		{
 			hlog::writef("", "  - fade %d", i);
@@ -182,7 +182,7 @@ void _test_handle_stream(xal::Player* player)
 			for_iter (j, 0, 10)
 			{
 				hthread::sleep(100);
-				xal::mgr->update(0.1f);
+				xal::manager->update(0.1f);
 			}
 		}
 		else if (i == 3)
@@ -192,99 +192,99 @@ void _test_handle_stream(xal::Player* player)
 			for_iter (j, 0, 10)
 			{
 				hthread::sleep(100);
-				xal::mgr->update(0.1f);
+				xal::manager->update(0.1f);
 			}
 		}
 	}
 	hlog::write("", "  - stop");
 	player->stop();
-	xal::mgr->update(0.1f);
+	xal::manager->update(0.1f);
 }
 
 void _test_fadein(xal::Player* player)
 {
 	hlog::write("", "  - start test fade in...");
-	xal::Player* p1 = xal::mgr->createPlayer(S_WIND);
+	xal::Player* p1 = xal::manager->createPlayer(S_WIND);
 	p1->play(1.0f);
 	for_iter (i, 0, 20)
 	{
 		hthread::sleep(100);
 		hlog::writef("", "T:%d P:%s FI:%s FO:%s", i, p1->isPlaying() ? "1" : "_", p1->isFadingIn() ? "1" : "_", p1->isFadingOut() ? "1" : "_");
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
 	p1->stop();
-	xal::mgr->update(1.0f);
+	xal::manager->update(1.0f);
 }
 
 void _test_fadeout(xal::Player* player)
 {
 	hlog::write("", "  - start test fade out...");
-	xal::Player* p1 = xal::mgr->createPlayer(S_WIND);
+	xal::Player* p1 = xal::manager->createPlayer(S_WIND);
 	p1->play();
 	p1->stop(1.0f);
 	for_iter (i, 0, 20)
 	{
 		hthread::sleep(100);
 		hlog::writef("", "T:%d P:%s FI:%s FO:%s", i, p1->isPlaying() ? "1" : "_", p1->isFadingIn() ? "1" : "_", p1->isFadingOut() ? "1" : "_");
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
 	p1->stop();
-	xal::mgr->update(0.1f);
+	xal::manager->update(0.1f);
 }
 
 void _test_fadeinout(xal::Player* player)
 {
 	hlog::write("", "  - start test fade in and out...");
-	xal::Player* p1 = xal::mgr->createPlayer(S_WIND);
+	xal::Player* p1 = xal::manager->createPlayer(S_WIND);
 	p1->play(1.0f);
 	for_iter (i, 0, 8)
 	{
 		hthread::sleep(100);
 		hlog::writef("", "T:%d P:%s FI:%s FO:%s", i, p1->isPlaying() ? "1" : "_", p1->isFadingIn() ? "1" : "_", p1->isFadingOut() ? "1" : "_");
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
 	p1->pause(1.0f);
 	for_iter (i, 0, 6)
 	{
 		hthread::sleep(100);
 		hlog::writef("", "T:%d P:%s FI:%s FO:%s", i, p1->isPlaying() ? "1" : "_", p1->isFadingIn() ? "1" : "_", p1->isFadingOut() ? "1" : "_");
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
 	p1->play(1.0f);
 	for_iter (i, 0, 3)
 	{
 		hthread::sleep(100);
 		hlog::writef("", "T:%d P:%s FI:%s FO:%s", i, p1->isPlaying() ? "1" : "_", p1->isFadingIn() ? "1" : "_", p1->isFadingOut() ? "1" : "_");
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
 	hlog::write("", "- 10 more updates");
 	for_iter (i, 0, 10)
 	{
 		hthread::sleep(100);
 		hlog::writef("", "T:%d P:%s FI:%s FO:%s", i, p1->isPlaying() ? "1" : "_", p1->isFadingIn() ? "1" : "_", p1->isFadingOut() ? "1" : "_");
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
 	p1->stop();
-	xal::mgr->update(1.0f);
+	xal::manager->update(1.0f);
 }
 
 void _test_complex_handler(xal::Player* player)
 {
 	hlog::write("", "  - start test complex handler...");
 	xal::Player* temp;
-	xal::Player* p1 = xal::mgr->createPlayer(S_WIND);
-	xal::Player* p2 = xal::mgr->createPlayer(S_WIND_2);
+	xal::Player* p1 = xal::manager->createPlayer(S_WIND);
+	xal::Player* p2 = xal::manager->createPlayer(S_WIND_2);
 	p1->play();
 	p2->play();
 	p2->pause();
 	for_iter (i, 0, 50)
 	{
 		hthread::sleep(100);
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 		p2->play();
 		p1->pause();
 		hthread::sleep(100);
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 		p2->pause();
 		p1->play();
 		if (i % 3 == 0)
@@ -296,45 +296,45 @@ void _test_complex_handler(xal::Player* player)
 	}
 	p1->stop();
 	p2->stop();
-	xal::mgr->destroyPlayer(p1);
-	xal::mgr->destroyPlayer(p2);
-	xal::mgr->update(0.1f);
+	xal::manager->destroyPlayer(p1);
+	xal::manager->destroyPlayer(p2);
+	xal::manager->update(0.1f);
 }
 
 void _test_memory_management(xal::Player* player)
 {
 	hlog::write("", "  - start test memory management...");
-	xal::mgr->setIdlePlayerUnloadTime(0.5f);
+	xal::manager->setIdlePlayerUnloadTime(0.5f);
 	player->play();
 	hthread::sleep(200);
-	xal::mgr->update(0.2f);
+	xal::manager->update(0.2f);
 	player->pause();
 	hthread::sleep(1000);
-	xal::mgr->update(1.0f);
+	xal::manager->update(1.0f);
 	player->stop();
 	player->play();
 	hthread::sleep(200);
-	xal::mgr->update(0.2f);
+	xal::manager->update(0.2f);
 	player->stop();
 	hlog::write("", "expecting automatic memory clearing now...");
 	hthread::sleep(1000);
-	xal::mgr->update(1.0f);
-	xal::Player* p1 = xal::mgr->createPlayer(S_WIND);
+	xal::manager->update(1.0f);
+	xal::Player* p1 = xal::manager->createPlayer(S_WIND);
 	p1->play();
 	player->play();
 	hthread::sleep(200);
-	xal::mgr->update(0.2f);
+	xal::manager->update(0.2f);
 	p1->pause();
 	player->pause();
 	hlog::write("", "trying to clear memory on paused sounds...");
-	xal::mgr->clearMemory();
+	xal::manager->clearMemory();
 	hthread::sleep(1000);
-	xal::mgr->update(0.5f);
+	xal::manager->update(0.5f);
 	p1->stop();
 	player->stop();
 	hlog::write("", "trying to clear memory on stopped sounds...");
-	xal::mgr->clearMemory();
-	xal::mgr->destroyPlayer(p1);
+	xal::manager->clearMemory();
+	xal::manager->destroyPlayer(p1);
 }
 
 void _test_sources(xal::Player* player)
@@ -342,24 +342,24 @@ void _test_sources(xal::Player* player)
 	hlog::write("", "  - start test sources...");
 	for_iter (i, 0, OPENAL_MAX_SOURCES + 1)
 	{
-		xal::mgr->play(S_BARK);
+		xal::manager->play(S_BARK);
 		hthread::sleep(20);
 	}
-	while (xal::mgr->isAnyPlaying(S_BARK))
+	while (xal::manager->isAnyPlaying(S_BARK))
 	{
 		hthread::sleep(100);
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
-	xal::mgr->update(0.01f);
-	xal::Player* p1 = xal::mgr->createPlayer(S_WIND);
+	xal::manager->update(0.01f);
+	xal::Player* p1 = xal::manager->createPlayer(S_WIND);
 	p1->play();
 	for_iter (i, 0, 20)
 	{
 		hthread::sleep(100);
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
 	p1->stop();
-	xal::mgr->update(0.1f);
+	xal::manager->update(0.1f);
 }
 
 void _test_util_playlist(xal::Player* player)
@@ -375,7 +375,7 @@ void _test_util_playlist(xal::Player* player)
 	while (list.isPlaying())
 	{
 		hthread::sleep(100);
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 		list.update();
 	}
 }
@@ -391,14 +391,14 @@ void _test_util_parallel_sounds(xal::Player* player)
 	for_iter (i, 0, 1000)
 	{
 		hthread::sleep(100);
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
 	names.clear();
 	pmgr.updateList(names);
 	for_iter (i, 0, 10)
 	{
 		hthread::sleep(100);
-		xal::mgr->update(0.1f);
+		xal::manager->update(0.1f);
 	}
 }
 
@@ -414,30 +414,30 @@ int main(Platform::Array<Platform::String^>^ args)
 	hwnd = GetConsoleWindow();
 #endif
 #ifndef _USE_THREADING
-	xal::init(xal::AS_DEFAULT, hwnd, false);
+	xal::init(xal::AudioSystemType::Default, hwnd, false);
 #else
-	xal::init(xal::AS_DEFAULT, hwnd, true, 0.01f);
+	xal::init(xal::AudioSystemType::Default, hwnd, true, 0.01f);
 #endif
 #ifdef _USE_MEMORY_MANAGEMENT
-	xal::mgr->createCategory("sound", xal::MANAGED, SOURCE_MODE);
+	xal::manager->createCategory("sound", xal::BufferMode::Managed, SOURCE_MODE);
 #endif
-	harray<hstr> files = xal::mgr->createSoundsFromPath(RESOURCE_PATH, "sound", "");
+	harray<hstr> files = xal::manager->createSoundsFromPath(RESOURCE_PATH, "sound", "");
 #ifndef _USE_LINKS
 #ifndef _USE_STREAM
-	xal::mgr->createCategory("streamable", xal::MANAGED, SOURCE_MODE);
+	xal::manager->createCategory("streamable", xal::BufferMode::Managed, SOURCE_MODE);
 #else
-	xal::mgr->createCategory("streamable", xal::STREAMED, SOURCE_MODE);
+	xal::manager->createCategory("streamable", xal::BufferMode::Streamed, SOURCE_MODE);
 #endif
-	files += xal::mgr->createSoundsFromPath(RESOURCE_PATH "streamable", "streamable", "");
+	files += xal::manager->createSoundsFromPath(RESOURCE_PATH "streamable", "streamable", "");
 #else
 #ifdef _USE_MEMORY_MANAGEMENT
-	xal::mgr->createCategory("cat", xal::MANAGED, SOURCE_MODE);
+	xal::manager->createCategory("cat", xal::BufferMode::Managed, SOURCE_MODE);
 #else
-	xal::mgr->createCategory("cat", xal::FULL, SOURCE_MODE);
+	xal::manager->createCategory("cat", xal::BufferMode::Full, SOURCE_MODE);
 #endif
-	xal::mgr->createSound(RESOURCE_PATH "linked/linked_sound.xln", "cat");
+	xal::manager->createSound(RESOURCE_PATH "linked/linked_sound.xln", "cat");
 #endif
-	xal::Player* player = xal::mgr->createPlayer(USED_SOUND);
+	xal::Player* player = xal::manager->createPlayer(USED_SOUND);
 
 #ifdef _TEST_BASIC
 	_test_basic(player);
@@ -476,7 +476,7 @@ int main(Platform::Array<Platform::String^>^ args)
 	_test_util_parallel_sounds(player);
 #endif
 
-	xal::mgr->destroyPlayer(player);
+	xal::manager->destroyPlayer(player);
 	hlog::write("", "  - done");
 	xal::destroy();
 #ifndef _WINRT
